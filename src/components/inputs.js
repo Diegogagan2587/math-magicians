c1ab929bc5515e23f0a68c5700ca4bdf410d0269
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-06 16:45:38 -0600

    refactor inputs.js: Add call event handler on each button instead of calling the handler in the parent

--- src/components/inputs.js
+++ src/components/inputs.js
@@ -1,25 +1,25 @@
 function CalcInputs({ eventHandler }) {
   return (
-    <div className="inputs-section" onClick={eventHandler}>
-      <button className="btn-ac" type="button">AC</button>
-      <button className="btn-plus-les" type="button">+/-</button>
-      <button className="btn-percentage" type="button">%</button>
-      <button className="divide orange" type="button">÷</button>
-      <button className="multiply orange" type="button">X</button>
-      <button className="less orange" type="button">-</button>
-      <button className="plus orange" type="button">+</button>
-      <button className="equal orange" type="button">=</button>
-      <button className="nine" type="button">9</button>
-      <button className="eigth" type="button">8</button>
-      <button className="seven" type="button">7</button>
-      <button className="six" type="button">6</button>
-      <button className="five" type="button">5</button>
-      <button className="four" type="button">4</button>
-      <button className="three" type="button">3</button>
-      <button className="two" type="button">2</button>
-      <button className="one" type="button">1</button>
-      <button className="zero" type="button">0</button>
-      <button className="point" type="button">.</button>
+    <div className="inputs-section">
+      <button onClick={eventHandler} className="btn-ac" type="button">AC</button>
+      <button onClick={eventHandler} className="btn-plus-les" type="button">+/-</button>
+      <button onClick={eventHandler} className="btn-percentage" type="button">%</button>
+      <button onClick={eventHandler} className="divide orange" type="button">÷</button>
+      <button onClick={eventHandler} className="multiply orange" type="button">X</button>
+      <button onClick={eventHandler} className="less orange" type="button">-</button>
+      <button onClick={eventHandler} className="plus orange" type="button">+</button>
+      <button onClick={eventHandler} className="equal orange" type="button">=</button>
+      <button onClick={eventHandler} className="nine" type="button">9</button>
+      <button onClick={eventHandler} className="eigth" type="button">8</button>
+      <button onClick={eventHandler} className="seven" type="button">7</button>
+      <button onClick={eventHandler} className="six" type="button">6</button>
+      <button onClick={eventHandler} className="five" type="button">5</button>
+      <button onClick={eventHandler} className="four" type="button">4</button>
+      <button onClick={eventHandler} className="three" type="button">3</button>
+      <button onClick={eventHandler} className="two" type="button">2</button>
+      <button onClick={eventHandler} className="one" type="button">1</button>
+      <button onClick={eventHandler} className="zero" type="button">0</button>
+      <button onClick={eventHandler} className="point" type="button">.</button>
     </div>
   );
 }
