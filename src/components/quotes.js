31665d9517cca49ccd7446b2a3c5f8a6c7d56317
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-07 16:08:15 -0600

    add loading sentence, add an error sentence

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,38 +1,43 @@
 import React, { useEffect, useState } from 'react';
 
 function Quotes() {
-  const [data, setData] = useState([]); 
+  const [data, setData] = useState([]);
   const dataURL = 'https://api.api-ninjas.com/v1/quotes';
   const keyAPI = 'nydBGDrsvveo1WS8DaxRHg==cvlsf9eU72JeSvDS';
   const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      const request = await fetch(dataURL, {
-        method: 'GET',
-        headers: { 'X-Api-Key': keyAPI },
-      });
-      const response = await request.json();
-      console.log(response[0]);
-      setData(response[0]);
+      try {
+        const request = await fetch(dataURL, {
+          method: 'GET',
+          headers: { 'X-Api-Key': keyAPI },
+        });
+        const response = await request.json();
+        setData(response[0]);
+      } catch (error) {
+        setHasError(true);
+      }
       setIsLoading(false);
     };
     fetchData();
-  }, [setData,setIsLoading]);
+  }, [setData, setIsLoading]);
 
-  if(isLoading) return (
-    <div className='loading'>Loading...</div>
-  );
+  if (hasError) return <div className='loading'>Something Went Wrong...!</div>
+
+
+  if (isLoading) return <div className='loading'>Loading...</div>
 
   return (
     <div className="section-quotes">
-        <h3>{`Author: ${data.author}`}</h3>
-        <p>
-          {`Category: ${data.category}`}
-        </p> 
-        <p>{data.quote}</p> 
-        
+      <h3>{`Author: ${data.author}`}</h3>
+      <p>
+        {`Category: ${data.category}`}
+      </p>
+      <p>{data.quote}</p>
+
     </div>
   )
 }
