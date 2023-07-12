f40dcf286d57e43397db7a0c7476d46c2a096e40
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-11 19:10:36 -0600

    add navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+const Navigation = () => {
+    return (
+      <nav>
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>
+            Calculator
+          </li>
+          <li>
+            Quote
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+
+  export default Navigation;
